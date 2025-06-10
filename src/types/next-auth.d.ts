c8589252b1f577e92ad50fa2d7bb4a9a35e71582
Jason Rossell <jasonrossell@Admins-MacBook-Pro-2.local>
2025-06-10 12:27:55 -0400

    Fix NextAuth type declarations for session.user.id TypeScript error

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -10,5 +10,14 @@ declare module "next-auth" {
 
   interface User {
     id: string
+    name?: string | null
+    email?: string | null
+    image?: string | null
   }
-} 
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    uid: string
+  }
+}
